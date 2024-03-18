bb8f7b28ae1327f7cdc00626b2b8049fe6ff8f93
gominie <109306648+gominie@users.noreply.com>
2024-03-18 18:34:52 +0000

    update if branch currently simplyfying methods

--- main.rb
+++ main.rb
@@ -5,6 +5,9 @@
 # guess a letter at a time
 # all guesses are saved into an array
 # is guess is correct its displayed amongst the _ _ _ _
+# need victory? method
+# need an array of guesses so far
+# only display
 
 class Hangman
   def initialize
@@ -15,42 +18,65 @@ class Hangman
       end
     end
     @secret_word = ''
-    @guess_count = 0
+    @guess_count = 10
     @guess = ''
     @placeholder = []
+    @victory = false
+    @guess_array = []
   end
 
   def word_generator
-    @secret_word = @words_arr.select { |word| word.length > 4 && word.length < 13 }.sample.split('')
+    # @secret_word = @words_arr.select { |word| word.length > 4 && word.length < 13 }.sample.split('')
+    @secret_word = %w[a a a]
   end
 
-  # def guess
-  # end
+  def update_placeholder
+    @secret_word.each_with_index do |_word, index|
+      # if @secret_word[index].include?(@guess) && !@guess_array.include?(@guess)
+      @placeholder[index] = @guess if @secret_word.include?(@guess) && !@guess_array.include?(@guess)
+    end
+  end
+
+  def check_guess_result
+    if @secret_word.include?(@guess) && !victory?
+      puts 'Good guess!'
+    elsif !@secret_word.include?(@guess) && !@guess_array.include?(@guess)
+      puts 'No luck!'
+      @guess_count -= 1
+    elsif @secret_word.include?(@guess) && @guess_array.include?(@guess)
+      puts "You've already guessed that letter!"
+    end
+    # puts "You've already guessed that letter!" if @guess_array.include?(@guess)
+    @guess_array << @guess unless @guess_array.include?(@guess)
+    p "You have #{@guess_count} incorrect guesses remaining" if @guess_count >= 1 && !@secret_word.include?(@guess)
+    puts "Letters guessed: #{@guess_array}" unless victory?
+    p @placeholder
+    puts 'Out of guesses' if @guess_count == 0 && !victory?
+  end
+
+  def victory?
+    return unless @placeholder === @secret_word
+
+    @victory = true
+    p 'You have won!'
+  end
 
   def handle_guess
     # later add in loop count
     @placeholder = Array.new(@secret_word.length, '_')
     p "secret word is #{@secret_word}"
     # text to make guess
-    while @guess_count < 10
+    while !@victory && @guess_count > 0
       p 'player enter your guess'
       @guess = gets.chomp
-      # @guess = ["a"]
       next unless @guess.length == 1 && @guess.respond_to?(:to_s)
 
-      @secret_word.each_with_index do |_word, index|
-        if @secret_word[index].include?(@guess)
-          @placeholder[index] = @guess
-        end
-      end
-      if !@secret_word.include?(@guess)
-        @guess_count += 1
-      end
-      p "You have #{@guess_count} incorrect guesses remaining"
-      p @placeholder
+      update_placeholder
+      check_guess_result
+      break if victory?
+
+
     end
-    # binding.pry
-    # p @placeholder
   end
 
   def start_game
